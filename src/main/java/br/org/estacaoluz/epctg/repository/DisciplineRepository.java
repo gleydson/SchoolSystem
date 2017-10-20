f74e043a0f5101723af8e5de70e37c54bfebf645
Gleydson Rodrigues <gleydsonsr@gmail.com>
2017-10-20 03:23:12 -0300

    update hard

--- src/main/java/br/org/estacaoluz/epctg/repository/DisciplineRepository.java
+++
@@ -1,7 +0,0 @@
-package br.org.estacaoluz.epctg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.org.estacaoluz.epctg.bean.Discipline;
-
-public interface DisciplineRepository extends JpaRepository<Discipline, Long> { }
--- src/main/java/br/org/estacaoluz/epctg/repository/SchoolClassRepository.java
+++
@@ -1,7 +0,0 @@
-package br.org.estacaoluz.epctg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.org.estacaoluz.epctg.bean.SchoolClass;
-
-public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long> { }
