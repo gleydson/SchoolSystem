e493a930de332c8091d07a0a793de651205fc79e
Gleydson Rodrigues <gleydsonsr@gmail.com>
2017-10-27 22:59:16 -0300

    create contact-information model

--- src/app/contact-information/contact-information.ts
+++ src/app/contact-information/contact-information.ts
@@ -1,3 +1,45 @@
 export class ContactInformation {
 
+    private _email : string;
+
+    constructor(
+        private _id : number,
+        private _phoneNumberOne : string,
+        private _phoneNumberTwo : string
+    ) {
+        this._email = null;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id : number) {
+        this._id = id;
+    }
+
+    get phoneNumberOne() {
+        return this._phoneNumberOne;
+    }
+
+    set phoneNumberOne(phoneNumberOne : string) {
+        this._phoneNumberOne = phoneNumberOne;
+    }
+
+    get phoneNumberTwo() {
+        return this._phoneNumberTwo;
+    }
+
+    set phoneNumberTwo(phoneNumberTwo : string) {
+        this._phoneNumberTwo = phoneNumberTwo;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(email : string) {
+        this._email = email;
+    }
+
 }
